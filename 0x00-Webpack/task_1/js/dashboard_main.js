e894a5f9ca9c8e354c7d7f0b457851840e07cdd3
hafsa chenni <hafsachenni77@gmail.com>
2024-06-24 21:56:53 +0200

    webpack config file generated part 2

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -8,7 +8,6 @@ $("body").append("<button>Click here to get started</button>");
 $("body").append("<p id='count'></p>");
 $("body").append("<p>Copyright - Holberton School</p>");
 
-
 let count = 0;
 
 function updateCounter(){
@@ -16,4 +15,4 @@ function updateCounter(){
     $("#count").text(`${count} clicks on the button`);
 }
 
-$("button").on("click", _.debounce(updateCounter, 700));
+$("button").on("click", _.debounce(updateCounter, 500));
