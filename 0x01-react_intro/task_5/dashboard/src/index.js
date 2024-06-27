700ef1a9b7885b0a7ec860ffa38450d1c2299ac8
hafsachenni <hafsachenni77@gmail.com>
2024-06-27 20:25:25 +0000

    webpack config done

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,12 +1,18 @@
+import $ from "jquery";
+import _ from "lodash";
 
 
-function components() {
-  const element = document.createElement('div');
-  element.innerHTML = 'Hello, webpack!';
-  element.classList.add('hello');
-  
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Dashboard data for the students</p>");
+$("body").append("<button>Click here to get started</button>");
+$("body").append("<p id='count'></p>");
+$("body").append("<p>Copyright - Holberton School</p>");
 
-  return element;
+let count = 0;
+
+function updateCounter(){
+    count += 1;
+    $("#count").text(`${count} clicks on the button`);
 }
 
-document.getElementById('root').appendChild(components());
+$("button").on("click", _.debounce(updateCounter, 500));
