32b1a8bde6034b215e63ed286e88c1a45b5bed93
hafsachenni <hafsachenni77@gmail.com>
2024-07-03 20:50:28 +0000

    new reusable components have been created

---
+++ 0x03-React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import BodySection from './BodySection';
+import './BodySectionWithMarginBottom.css';
+
+const BodySectionWithMarginBottom = (props) => {
+  return (
+    <div className="bodySectionWithMargin">
+      <BodySection {...props} />
+    </div>
+  );
+};
+
+
+BodySectionWithMarginBottom.defaultProps = {
+   title: '',
+};
+
+BodySectionWithMarginBottom.propTypes = {
+   title: PropTypes.string,
+};
+
+export default BodySectionWithMarginBottom;
