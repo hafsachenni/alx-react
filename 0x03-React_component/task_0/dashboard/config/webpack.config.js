1a0c209c5a11e9d27da6a3b566c384f8366752ea
hafsachenni <hafsachenni77@gmail.com>
2024-07-03 16:19:22 +0000

    changed component to a class componenrt

--- 0x03-React_component/task_0/dashboard/config/webpack.config.js
+++ 0x03-React_component/task_0/dashboard/config/webpack.config.js
@@ -3,19 +3,14 @@ const path = require('path');
 module.exports = {
   mode: 'development',
   devtool: 'inline-source-map',
-  entry: './src/index.js',
+  entry: './src/index.js',  
   output: {
-<<<<<<< HEAD
-    filename: 'bundle.js',
-=======
     filename: 'bundle.js', 
->>>>>>> f0e454633275ccc149aa843bec9a7e511ff78487
     path: path.resolve('./dist'),
   },
   devServer: {
     hot: true,
-    contentBase: path.resolve('./dist'),
-    compress: true,
+    static: path.resolve('./dist'),
     port: 3000,
   },
   module: {
@@ -27,22 +22,22 @@ module.exports = {
       {
         test: /\.(jpg|png)$/i,
         use: [
-          'file-loader',
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true,
-              disable: true,
+            'file-loader',
+            {
+                loader: 'image-webpack-loader',
+                options: {
+                    bypassOnDebug: true,
+                    disable: true,
+                },
             },
-          },
         ],
       },
       {
-        test: /\.jsx?$/i,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+	test: /\.jsx?$/i,
+	exclude: /node_modules/,
+	use: {
+	  loader: 'babel-loader',
+	},
       },
     ],
   },
