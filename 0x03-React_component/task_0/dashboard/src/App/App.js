daad1e9900aa73782f8dc2cf57736214e444e4e2
hafsachenni <hafsachenni77@gmail.com>
2024-07-03 14:20:48 +0000

    changed app.js from a functional component to a class component

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -7,32 +7,32 @@ import CourseList from '../CourseList/CourseList';
 import PropTypes from 'prop-types';
 import { getLatestNotification } from '../utils/utils';
 
-function App({ isLoggedIn }) {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-  ];
-  class App extends Component {
-	constructor(props) {
-	  super(props);
-        }
-	render(){
-	const { isLoggedIn } = this.props;
-	return (
-	  <Fragment>
-            <Notifications listNotifications={listNotifications} />
-	    <Header />
-	    {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
-	    <Footer />
-	  </Fragment>
-        );
-      }
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const { isLoggedIn } = this.props;
+    return (
+      <Fragment>
+        <Notifications listNotifications={listNotifications} />
+        <Header />
+        {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+        <Footer />
+      </Fragment>
+    );
+  }
 }
 
 App.defaultProps = {
