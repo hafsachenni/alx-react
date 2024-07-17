1a1a39d361f8a089c04810dd36be1fa89f1f9d0a
hafsa chenni <hafsachenni77@gmail.com>
2024-07-17 23:02:04 +0200

    lazy sequence created

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,12 @@
+import { Seq } from 'immutable';
+
+export default function printBestStudents (grades) {
+  const studentSeqnc = Seq(grades);
+  const scores = studentSeqnc.filter(student => student.score >= 70).map(student => ({
+    ...student,
+    firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1).toLowerCase(),
+    lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1).toLowerCase()
+  }))
+    .toObject();
+  console.log(scores);
+}
