35176ea243fd1db515562a81d84d70efa2dd9f04
hafsa chenni <hafsachenni77@gmail.com>
2024-08-20 20:46:16 +0200

    actions bounded

---
+++ 0x07-react_redux_action_creator_normalizr/task_6/dashboard/src/BodySection/BodySection.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import BodySection from './BodySection';
+
+describe('<BodySection />', () => {
+  test('checking that shallowing the component should render correctly the children and one h2 element', () => {
+    const wrapper = shallow(
+      <BodySection title="test title">
+        <p>test children node</p>
+      </BodySection>
+    );
+
+    expect(wrapper.find('h2').length).toBe(1);
+    expect(wrapper.find('h2').text()).toBe('test title');
+
+    expect(wrapper.find('p').length).toBe(1);
+    expect(wrapper.find('p').text()).toBe('test children node');
+  });
+});
