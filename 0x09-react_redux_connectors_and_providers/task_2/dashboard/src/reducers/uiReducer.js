8e185e2477c43868585a08b2bb95e461605f5816
hafsachenni <hafsachenni77@gmail.com>
2024-08-30 14:02:46 +0100

    uiReducer modified

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/reducers/uiReducer.js
@@ -1,10 +1,12 @@
 import {LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE} from "../actions/uiActionTypes";
 import { Map } from "immutable";
+import { logout } from "../actions/uiActionCreators";
+
 
 export const initialState = {
     isNotificationDrawerVisible : false,
     isUserLoggedIn : false,
-    user: {},
+    user: null,
 };
 
 
@@ -23,7 +25,7 @@ const uiReducer = (state = Map(initialState), action) => {
             return state.set("isUserLoggedIn", false);
             
         case LOGOUT:
-            return state.set("isUserLoggedIn", false);
+            return state.set("isUserLoggedIn", false).set("user", null);
     default:
         return state;
         }
