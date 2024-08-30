b40460ab00df2aa65e0a3640b50e6c77dfaf6516
hafsachenni <hafsachenni77@gmail.com>
2024-08-30 15:30:26 +0100

    new action creators

---
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/schema/notifications.js
@@ -0,0 +1,30 @@
+import * as notifsData from "../../notifications.json";
+import { schema, normalize} from 'normalizr';
+
+
+const user = new schema.Entity("users");
+const message = new schema.Entity("messages", {}, {
+    idAttribute: "guid",
+});
+const notification = new schema.Entity("notifications", {
+    author: user,
+    context: message,
+});
+
+const normalizeData = normalize(notifsData.default, [notification]);
+export { normalizeData };
+
+
+
+export function getAllNotificationsByUser(userId) {
+    const { result, entities } = normalizeData;
+    return result.filter((id) => entities.notifications[id].author === userId)
+                 .map((id) => entities.notifications[id].context);
+}
+
+const notificationsNormalizer = (data) => {
+    const notifNormalizer = normalize(data, [notification]);
+    return notifNormalizer.entities;
+};
+
+export default notificationsNormalizer;
